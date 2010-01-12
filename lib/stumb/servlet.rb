20db4a538ed013c2f145de404a8d5eb17211db02
moro <moronatural@gmail.com>
2010-01-12 21:19:35 +0900

    add mocking

--- lib/stumb/servlet.rb
+++ lib/stumb/servlet.rb
@@ -1,10 +1,56 @@
 require 'sinatra'
+require 'monitor'
 
 module Stumb
   class Servlet < Sinatra::Base
     module Double
-      def stub(verb, path, &block)
-        send(verb, path, &block)
+      class Error < RuntimeError; end
+
+      def self.extended(base)
+        base.extend Stub
+        base.extend Mock
+      end
+
+      module Mock
+        def mock(verb, path, &block)
+          v = verb.to_s.upcase
+          ident = [verb, path]
+          expectations << ident
+
+          action = Proc.new do |*args|
+            self.class.consume(ident)
+            instance_eval(&block)
+          end
+          stub(verb, path, &action)
+        end
+
+        def finish
+          raise Error unless expectations.empty?
+        end
+
+        # TODO refactor
+        def consume(ident)
+          synchronize do
+            pos = expectations.index(expectations.detect {|m| m.eql?(ident) })
+            expectations.delete_at(pos)
+          end
+        end
+
+        private
+        def expectations
+          @expectations ||= []
+        end
+      end
+
+      module Stub
+        def stub(verb, path, &block)
+          v = verb.to_s.upcase
+
+          synchronize do
+            send(verb, path, &block)
+            routes[v].unshift(routes[v].pop)
+          end
+        end
       end
     end
 
@@ -20,6 +66,7 @@ module Stumb
       def base(&block)
         Class.new(self).tap do |k|
           k.extend Double
+          k.extend MonitorMixin
           k.class_eval(&block)
         end
       end
--- spec/stumb/servlet_spec.rb
+++ spec/stumb/servlet_spec.rb
@@ -15,12 +15,9 @@ describe Stumb::Servlet do
   it { should be_instance_of Class }
 
   describe "GET /" do
-    before do
-      @response = @app.new.call(
-        Rack::MockRequest.env_for("/", :method => "GET")
-      )
+    subject do
+      @app.new.call( Rack::MockRequest.env_for("/", :method => "GET"))
     end
-    subject{ @response }
 
     it { should == [200, {"Content-Type"=>"text/plain", "Content-Length"=>"11"}, ["Hello World"]] }
   end
@@ -35,12 +32,53 @@ describe Stumb::Servlet do
     end
 
     subject{
-      @app.new.call(
-        Rack::MockRequest.env_for("/dynamic_add", :method => "GET")
-      )
+      @app.new.call( Rack::MockRequest.env_for("/dynamic_add", :method => "GET"))
     }
 
     it { should == [200, {"Content-Type"=>"text/plain", "Content-Length"=>"8"}, ["Hi World"]] }
+  end
+
+  describe "stub(:get, '/') # override" do
+    before do
+      @app.stub(:get, '/') do
+        response.status = 200
+        response["Content-Type"] = "text/plain"
+        response.body = "Hi World"
+      end
+    end
+
+    subject{
+      @app.new.call( Rack::MockRequest.env_for("/", :method => "GET"))
+    }
+
+    it { should == [200, {"Content-Type"=>"text/plain", "Content-Length"=>"8"}, ["Hi World"]] }
+  end
+
+  describe "mock(:get, '/')" do
+    before do
+      @app.mock(:get, '/') do
+        response.status = 200
+        response["Content-Type"] = "text/plain"
+        response.body = "Hi World"
+      end
+    end
+
+    describe "call" do
+      before do
+        @response = @app.new.call( Rack::MockRequest.env_for("/", :method => "GET"))
+      end
+
+      subject{ @response }
+
+      it { should == [200, {"Content-Type"=>"text/plain", "Content-Length"=>"8"}, ["Hi World"]] }
+      it {
+        expect{ @app.finish }.should_not raise_error Stumb::Servlet::Double::Error
+      }
+    end
+
+    describe "don't call" do
+      it { expect{ @app.finish }.should raise_error Stumb::Servlet::Double::Error }
+    end
   end
 end
 

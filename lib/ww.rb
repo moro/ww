fce1e835c921c4e41333e26c98e5bef4523dcc42
moro <moronatural@gmail.com>
2010-01-25 11:53:22 +0900

    implement new API. this is pre of 0.4.0

--- lib/ww.rb
+++ lib/ww.rb
@@ -1,17 +1,4 @@
 module Ww
-  Version = '0.3.1'
-
-  def to_app(*args, &block)
-    $stderr.puts <<-WORNING
-*** DUPLICATION WORNING ***
-Ww.to_app moves to Ww::SpyEye.to_app
-
-This comatibility will be lost on 0.4.0.
-***************************
-WORNING
-    require 'ww/spy_eye'
-    Ww::SpyEye.to_app(*args, &block)
-  end
-  module_function :to_app
+  Version = '0.3.9'
 end
 
